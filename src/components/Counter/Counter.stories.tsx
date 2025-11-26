ce26a07acd11bae6ed76d0c6cd248ba643266b99
Ronald Rey <reyronald@gmail.com>
2025-11-26 15:38:40 -0400

    sb test

--- src/components/Counter/Counter.stories.tsx
+++ src/components/Counter/Counter.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite"
+import { userEvent, within } from "@storybook/test"
 
 import { Counter } from "./Counter"
 
@@ -14,4 +15,26 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default = {} satisfies Story
+export const Default = {
+  play: async (context) => {
+    const canvas = within(context.canvasElement)
+
+    canvas.getByText("0")
+
+    const incrementButton = canvas.getByRole("button", { name: "Increment" })
+    await userEvent.click(incrementButton)
+    canvas.getByText("1")
+    await userEvent.click(incrementButton)
+    await userEvent.click(incrementButton)
+    canvas.getByText("3")
+
+    const decrementButton = canvas.getByRole("button", { name: "Decrement" })
+    await userEvent.click(decrementButton)
+    canvas.getByText("2")
+
+    await userEvent.click(canvas.getByRole("button", { name: "Reset" }))
+    canvas.getByText("0")
+
+    await userEvent.click(decrementButton)
+  },
+} satisfies Story
